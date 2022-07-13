5cef5e23b2cb97df74aa637cca975d004726fb41
Andreza Zeverino <andrezazeverino@gmail.com>
2022-07-13 12:39:21 -0300

    Criação do projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import banner from '../../assets/images/banner.png'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+       <img src={banner} alt="teste" /> </header>
+    )
+}
+
+export default Banner 
